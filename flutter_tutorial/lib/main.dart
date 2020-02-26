2baaaab4bbf0589e48c93210197f1a79ab6a934c
David Spano <david.spano@move.com>
2020-02-26 08:18:11 -0500

    Added Date Picker

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(new MaterialApp(
@@ -12,6 +13,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  String _value = '';
+
+  Future _selectDate() async { // async keyword is very important
+    DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: new DateTime(2016),
+        lastDate: new DateTime(2021)
+    );
+
+    if (picked != null) setState(() => _value = picked.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -23,7 +37,8 @@ class _State extends State<MyApp> {
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new Text('Hello World')
+              new Text(_value),
+              new RaisedButton(onPressed: _selectDate, child: new Text("Click Me"))
             ]
           )
         )
@@ -31,3 +46,20 @@ class _State extends State<MyApp> {
     );
   }
 }
+
+/**
+ *                    * Date Picker *
+ *
+    Future _selectDate() async { // async keyword is very important
+      DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: new DateTime(2016),
+        lastDate: new DateTime(2021)
+      );
+
+      if (picked != null) setState(() => _value = picked.toString());
+    }
+
+*/
+
