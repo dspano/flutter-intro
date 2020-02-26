91d45aa1605bc1065d23a6a3d5e6cde26d51374e
David Spano <david.spano@move.com>
2020-02-26 08:47:22 -0500

    Persistent Footer Buttons

--- flutter_tutorial/lib/main.dart
+++ flutter_tutorial/lib/main.dart
@@ -13,30 +13,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  String _value = "";
+
+  void _onClick(String value) => setState(() => _value = value);
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Name here'),
       ),
-      drawer: new Drawer(
-        child: new Container(
-          padding: new EdgeInsets.all(32.0),
-          child: new Column(
-            children: <Widget>[
-              new Text("Hello Drawer"),
-              new RaisedButton(onPressed: () => Navigator.pop(context), child: new Text("Close"))
-            ],
-          )
-        )
-      )
-      ,
+      persistentFooterButtons: <Widget>[
+        new IconButton(icon: new Icon(Icons.timer), onPressed: () => _onClick("Button 1")),
+        new IconButton(icon: new Icon(Icons.people), onPressed: () => _onClick("Button 2")),
+        new IconButton(icon: new Icon(Icons.ac_unit), onPressed: () => _onClick("Button 3"))
+      ],
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new Text("Text Goes Here"),
+              new Text(_value),
             ]
           )
         )
@@ -104,5 +100,15 @@ class _State extends State<MyApp> {
         )
       )
     )
- */
+  */
 
+/**
+ *                * Persistent Footer Buttons *
+ *
+    persistentFooterButtons: <Widget>[
+      new IconButton(icon: new Icon(Icons.timer), onPressed: () => _onClick("Button 1")),
+      new IconButton(icon: new Icon(Icons.people), onPressed: () => _onClick("Button 2")),
+      new IconButton(icon: new Icon(Icons.ac_unit), onPressed: () => _onClick("Button 3"))
+    ]
+  *
+  */
